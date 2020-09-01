fb6c7d0a4886279aca530b6c91f707206e360428
PondusEin <PondusEin@users.noreply.github.com>
2020-09-01 14:17:48 +0200

    Add project files.

---
+++ Age question/Age question.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+int user_age;
+
+
+int main()
+{
+	std::cout << "How old are you?\n";
+	std::cin >> user_age;
+
+	if (user_age >= 20) {
+		std::cout << "Wow, you are an adult!";
+	}
+	if (user_age < 20) {
+		std::cout << "Wowie, you're a kid!\n" << "Fuck off.";
+	}
+}
